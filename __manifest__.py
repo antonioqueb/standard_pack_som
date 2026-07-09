f5b47288e041991e08ecbfa6e31c1476949be91e
Antonio Queb <cardstillo@gmail.com>
2026-07-09 09:04:05 -0600

    Actualiza la versión del módulo a 19.0.1.5.0. Agrega campos para el cálculo de cierre en cajas completas y su visualización en la interfaz de pedido de venta.

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'StandardPack',
-    'version': '19.0.1.4.1',
+    'version': '19.0.1.5.0',
     'category': 'Sales',
     'summary': 'Venta por empaque estándar con cálculo vaivén paquetes ↔ cantidad',
     'description': """
